a4cbc89630b5b342ecf55fc7f688a9abe0ac5667
lggg123 <machinelearner2334@gmail.com>
2024-12-25 16:31:06 -0800

    fix: removed CA

--- src/app/components/ContractInfo.tsx
+++ src/app/components/ContractInfo.tsx
@@ -21,7 +21,7 @@ const ContractInfo = () => {
         onClick={handleCopy}
         className="bg-teal-900 text-white text-center p-4 rounded-2xl border-4 border-white w-11/12 max-w-2xl hover:bg-teal-800 transition-colors"
       >
-        CA: {contractAddress}
+        {contractAddress}
         {copied && <span className="ml-2">(Copied!)</span>}
       </button>
     </div>
